d28d5e8881feddf8ae45e0975c2302254f0e767c
Adam Vesecký <vesecky.adam@gmail.com>
2022-10-18 01:59:57 +0200

    Add Queue

--- README.md
+++ README.md
@@ -6,21 +6,26 @@ Our newest task is another variation of the traveling salesman problem that is i
 Salesman world rules
 - There is a random m x n grid (i.e. 30 x 20), consists from 1 x 1 square cells. This is salesman’s world.
 - Salesman has no map when he appears in the world. That means he doesn’t know about the cities and walls location in advance and has to explore the map at first
-    Salesman is allowed to travel one cell at time - up, down, left and right. Not diagonally.
-    Anyway, Salesman can see what is in the all the adjacent cells around him, including diagonal ones.
-    Cell represents either the road (grass), the city, or the wall
-    Salesman cannot hit the wall, god mode is not allowed :slightly_smiling_face:
-    Salesman will appear in random city at the start
+- Salesman is allowed to travel one cell at time - up, down, left and right. Not diagonally.
+- Anyway, Salesman can see what is in the all the adjacent cells around him, including diagonal ones.
+- Cell represents either the road (grass), the city, or the wall
+- Salesman cannot hit the wall, god mode is not allowed
+- Salesman will appear in random city at the start
 
-Objectives - those are dependent, so start from #1, then #2 and then #3 :slightly_smiling_face:
-
-    Explore the whole map from the entry city to explore the whole world in the most effective way possible - by this, I mean to make sure every cell is visited.
-    When the exploration step is completed, find the shortest path back to the very first city where the salesman appeared.
-    When the salesman is back, the ultimate goal is to evaluate all costs (distance) permutations and pick the cheapest one so the salesman can save as much time as possible during his next trip, while visiting all cities again. EACH CITY CAN BE VISITED EXACTLY ONCE.
+Objectives - those are dependent, so start from #1, then #2 and then #3
+- Explore the whole map from the entry city to explore the whole world in the most effective way possible - by this, I mean to make sure every cell is visited.
+- When the exploration step is completed, find the shortest path back to the very first city where the salesman appeared.
+- When the salesman is back, the ultimate goal is to evaluate all costs (distance) permutations and pick the cheapest one so the salesman can save as much time as possible during his next trip, while visiting all cities again. EACH CITY CAN BE VISITED EXACTLY ONCE.
 
 Additional notes
+- There was a discussion about the potential visualisation using some graphical UI. This is completely optional, but nice to have.
+- There are no rules what tech stack you have to use. Use whatever you want.
+- Use any graph algorithms you believe will be suitable for those 3 steps (exploring, shortest path back, shortest possible trip route).
+- HINT: The last one will be the most complex and there is no need to be shy to use brute force :slightly_smiling_face: More info about the problem: https://www.geeksforgeeks.org/travelling-salesman-problem-set-1/
 
-    There was a discussion about the potential visualisation using some graphical UI. This is completely optional, but nice to have.
-    There are no rules what tech stack you have to use. Use whatever you want.
-    Use any graph algorithms you believe will be suitable for those 3 steps (exploring, shortest path back, shortest possible trip route).
-    HINT: The last one will be the most complex and there is no need to be shy to use brute force :slightly_smiling_face: More info about the problem: https://www.geeksforgeeks.org/travelling-salesman-problem-set-1/
+
+The best approach (I am aware of) consists of running a 
+- Depth-First Branch-and-Bound heuristic search algorithm where the heuristic is the cost of the Minimum Spanning Tree (MST). 
+- Since the MST can be computed in polynomial time with either the Prim's algorithm or the Kruskal's algorithm, 
+- then it can be expected to return solutions in a reasonable amount of time. 
+- For a wonderful discussion of these two algorithms I do strongly suggest you to have a look at The Algorithm Design Manual
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ let WIDTH = 30;
 let HEIGHT = 20;
 
 let arr = [];
-const BLOCK_EMPTY = 0;
-const BLOCK_WALL = 1;
-const BLOCK_CITY = 2;
+const BLOCK_UNDEFINED = 0;
+const BLOCK_ROAD = 1;
+const BLOCK_WALL = 2;
+const BLOCK_CITY = 3;
 
 const salesman = {
     x: 0,
@@ -21,59 +22,134 @@ const salesman = {
                 arr.push(BLOCK_WALL);
             }
         } else {
-            arr.push(BLOCK_EMPTY);
+            arr.push(BLOCK_ROAD);
         }
     }
 }*/
 
 WIDTH = HEIGHT = 6;
 arr = [
-    0,0,0,0,0,0,
-    0,1,0,2,0,0,
-    2,0,0,0,0,1,
-    1,1,1,1,0,1,
-    0,0,0,0,0,2,
-    2,0,1,0,0,0,
+    1, 1, 1, 1, 1, 1,
+    1, 2, 1, 3, 1, 1,
+    3, 1, 1, 1, 1, 2,
+    2, 2, 2, 2, 1, 2,
+    1, 1, 1, 1, 1, 3,
+    3, 1, 2, 1, 1, 1,
 ];
 
-const print = () => {
+const print = (array = arr) => {
     let otp = "";
-    arr.forEach((val, index) => {
+    array.forEach((val, index) => {
         otp += `${val}`;
-        if(index !== 0 && ((index + 1) % WIDTH) === 0) {
-            otp +='\n';
+        if (index !== 0 && ((index + 1) % WIDTH) === 0) {
+            otp += '\n';
         }
     });
     console.log(otp);
 }
 
 const coordToIndex = (x, y) => {
+    if(x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT) {
+        return -1;
+    }
     return y * WIDTH + x;
 }
 
 const indexToCoord = (x) => {
-    return [x % WIDTH, Math.floor(x / WIDTH)];
+    return {x: x % WIDTH, y: Math.floor(x / WIDTH)};
 }
 
 const putSalesmanIntoMap = () => {
     console.log('Put into map');
-    const zeroes = arr.filter(a => a === 0).length;
-    console.log('Zeroes: ' + zeroes);
-    //const randomLoc = Math.floor(Math.random() * zeroes);
-    const randomLoc = 14;
+    const cities = arr.filter(a => a === BLOCK_CITY).length;
+    console.log('Cities: ' + cities);
+    //const randomLoc = Math.floor(Math.random() * cities);
+    const randomLoc = 2;
     console.log('Random loc: ' + randomLoc);
     let cnt = 0;
     const index = arr.findIndex((val, index) => {
-        if(val === BLOCK_EMPTY && ++cnt === randomLoc) {
+        if (val === BLOCK_CITY && ++cnt === randomLoc) {
             return true;
         }
         //cnt++;
     });
     const coord = indexToCoord(index);
-    salesman.x = coord[0];
-    salesman.y = coord[1];
+    salesman.x = coord.x;
+    salesman.y = coord.y;
     console.log('Found pos: ', index, salesman.x, salesman.y);
+}
+
+const getNeighbors = (x, y, diagonal = true) => {
+    if(diagonal) {
+        return [
+            arr[coordToIndex(x - 1, y - 1)], arr[coordToIndex(x, y - 1)], arr[coordToIndex(x + 1, y - 1)],
+            arr[coordToIndex(x - 1, y)], arr[coordToIndex(x, y)], arr[coordToIndex(x + 1, y)],
+            arr[coordToIndex(x - 1, y + 1)], arr[coordToIndex(x, y + 1)], arr[coordToIndex(x + 1, y + 1)]
+        ].map(v => v ?? BLOCK_UNDEFINED)
+    } else {
+        return [
+            BLOCK_UNDEFINED, arr[coordToIndex(x, y - 1)], BLOCK_UNDEFINED,
+            arr[coordToIndex(x - 1, y)], arr[coordToIndex(x, y)], arr[coordToIndex(x + 1, y)],
+            BLOCK_UNDEFINED, arr[coordToIndex(x, y + 1)], BLOCK_UNDEFINED
+        ].map(v => v ?? BLOCK_UNDEFINED)
+    }
+}
+
+const exploreMap = () => {
+    let exarr = [];
+    for (let i = 0; i < WIDTH; i++) {
+        for (let j = 0; j < HEIGHT; j++) {
+            exarr.push(BLOCK_UNDEFINED);
+        }
+    }
+
+    exarr[coordToIndex(salesman.x, salesman.y)] = BLOCK_CITY;
+    print(exarr);
+
+    const NODE_NEW = 0;
+    const NODE_EXPLORED = 1;
+    const helpArray = [...exarr].map(val => NODE_NEW);
+    const queue = [];
+    queue.push(salesman);
+    // starting node is explored
+    helpArray[coordToIndex(salesman.x, salesman.y)] = NODE_EXPLORED;
+
+    console.log('Searching');
+    print(helpArray);
+    while(queue.length !== 0) {
+        const qCoord = queue[queue.length - 1];
+        queue.splice(queue.length - 1, 1);
+        // get all neighbors including diagonals
+        const neighbors = getNeighbors(qCoord.x, qCoord.y, true);
+        neighbors.forEach((val, index) => {
+            // get neighbor coord
+            const coord = {x: index % 3, y: Math.floor(index / 3) };
+            const isDiagonal = index === 0 || index === 2 || index === 6 || index === 8;
+            const isCurrent = index === 4; // we can skip the current node
+
+            if(!isCurrent && val !== BLOCK_UNDEFINED) {
+                const bigMapCoord = { // [1, 1] is the center
+                    x: qCoord.x + coord.x - 1,
+                    y: qCoord.y + coord.y - 1
+                };
+                const bigMapIndex = coordToIndex(bigMapCoord.x, bigMapCoord.y);
+                if(!isDiagonal && helpArray[bigMapIndex] !== NODE_EXPLORED) {
+                    if(val !== BLOCK_WALL) {
+                        queue.push(bigMapCoord);
+                    }
+                    helpArray[bigMapIndex] = NODE_EXPLORED;
+                }
+                // we can look diagonally, but can't go there
+                // if there is a wall, we can ignore it
+                if(isDiagonal && val === BLOCK_WALL) {
+                    helpArray[bigMapIndex] = NODE_EXPLORED;
+                }
+            }
+        });
+        print(helpArray);
+    }
 }
 
 print();
 putSalesmanIntoMap();
+exploreMap();
