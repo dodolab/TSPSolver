ca9c43f0e1aa50d216a7c6b5fc583161d9785680
Adam Vesecký <vesecky.adam@gmail.com>
2022-10-18 02:01:04 +0200

    Add TS Types

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,250 @@
+type Neighbor = 'left' | 'right' | 'top' | 'bottom' | 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
+
+class Coord {
+	private _x: number;
+	private _y: number;
+
+	constructor(x: number, y: number) {
+		this._x = x;
+		this._y = y;
+	}
+
+	get x() {
+		return this._x;
+	}
+
+	get y() {
+		return this._y;
+	}
+
+	add(coord: Coord) {
+		return new Coord(this.x + coord.x, this.y + coord.y);
+	}
+
+	left() {
+		return new Coord(this.x - 1, this.y);
+	}
+
+	right() {
+		return new Coord(this.x + 1, this.y);
+	}
+
+	top() {
+		return new Coord(this.x, this.y-1);
+	}
+
+	bottom() {
+		return new Coord(this.x, this.y + 1);
+	}
+
+	topLeft() {
+		return new Coord(this.x - 1, this.y - 1);
+	}
+
+	topRight() {
+		return new Coord(this.x + 1, this.y - 1);
+	}
+
+	bottomLeft() {
+		return new Coord(this.x - 1, this.y + 1);
+	}
+
+	bottomRight() {
+		return new Coord(this.x + 1, this.y + 1);
+	}
+}
+
+type TileType = 'UNKNOWN' | 'ROAD' | 'WALL' | 'CITY';
+
+
+class MapTile {
+	neighbors: Record<Neighbor, MapTile>;
+	// directional neighbors in an array. Can be null if there is no such neighbour!
+	directionalNeighbors: [MapTile, MapTile, MapTile, MapTile];
+	coord: Coord;
+	type: TileType;
+
+	constructor(coord: Coord, type: TileType) {
+		this.coord = coord;
+		this.type = type;
+	}
+
+	get isWalkable() {
+		return this.type === 'ROAD' || this.type === 'CITY';
+	}
+}
+
+class MapGrid {
+	mapArray: MapTile[] = [];
+	width: number;
+	height: number;
+
+	constructor(width: number, height: number) {
+		this.width = width;
+		this.height = height;
+	}
+		
+	coordToIndex = (coord: Coord): number => {
+		const { x, y } = coord;
+		if (x < 0 || y < 0 || x >= this.width || y >= this.height) {
+			return -1;
+		}
+		return y * this.width + x;
+	}
+
+	indexToCoord = (x: number): Coord => {
+		return new Coord(x % this.width, Math.floor(x / this.width));
+	}
+
+	isInside = (coord: Coord) => {
+		return coord.x >= 0 && coord.y >= 0 && coord.x < this.width && coord.y < this.height;
+	}
+
+	setTile(coord: Coord, type: TileType) {
+		this.mapArray[this.coordToIndex(coord)] = new MapTile(coord, type);
+	}
+
+	getTile(coord: Coord) {
+		if(!this.isInside(coord)) {
+			return null;
+		}
+		return this.mapArray[this.coordToIndex(coord)];
+	}
+
+	generateNeighbors() {
+		for(let tile of this.mapArray) {
+			// tile will contain links to all neighbors
+			tile.neighbors = {
+				left: this.getTile(tile.coord.left()),
+				right: this.getTile(tile.coord.right()),
+				top: this.getTile(tile.coord.top()),
+				bottom: this.getTile(tile.coord.bottom()),
+				topLeft: this.getTile(tile.coord.topLeft()),
+				topRight: this.getTile(tile.coord.topRight()),
+				bottomLeft: this.getTile(tile.coord.bottomLeft()),
+				bottomRight: this.getTile(tile.coord.bottomRight()),
+			}
+			// this order is very important!!!
+			tile.directionalNeighbors = [tile.neighbors.top, tile.neighbors.right, tile.neighbors.bottom, tile.neighbors.left]
+		}
+	}
+
+	print = () => {
+		let otp = "";
+		this.mapArray.forEach((val, index) => {
+			switch(val.type) {
+				case 'CITY':
+					otp += 'o';
+				break;
+				case 'WALL':
+					otp += 'x';
+				break;
+				case 'ROAD':
+					otp += '.';
+				break;
+				case 'UNKNOWN':
+					otp += '?';
+				break;
+			}
+			if (index !== 0 && ((index + 1) % this.width) === 0) {
+				otp += '\n';
+			}
+		});
+		console.log(otp);
+	}
+}
+
+
+// todo we only have priorities 0 and 1 ...
 class PriorityQueue {
-	collection = [];
+	collection: [Coord, number] [] = [];
 
 	// we store [index, priority]
-	enqueue(element) {
-		if (this.isEmpty()) {
-			this.collection.push(element);
+	enqueue(element: Coord, priority: number = 0) {
+		if (this.isEmpty) {
+			this.collection.push([element, priority]);
 		} else {
 			let added = false;
 			for (let i = 1; i <= this.collection.length; i++) {
-				if (element[1] < this.collection[i - 1][1]) {
-					this.collection.splice(i - 1, 0, element);
+				if (priority < this.collection[i - 1][1]) {
+					this.collection.splice(i - 1, 0, [element, priority]);
 					added = true;
 					break;
 				}
 			}
 			if (!added) {
-				this.collection.push(element);
+				this.collection.push([element, priority]);
 			}
 		}
-	};
+	}
+
 	dequeue() {
 		let value = this.collection.shift();
 		return value;
-	}; isEmpty() {
+	}
+
+	get isEmpty() {
 		return (this.collection.length === 0)
-	};
+	}
 }
 
+const WIDTH = 30;
+const HEIGHT = 20;
 
-let WIDTH = 30;
-let HEIGHT = 20;
+class Salesman {
+	coord: Coord;
+	map: MapGrid;
 
-let arr = [];
-const BLOCK_UNDEFINED = 0;
-const BLOCK_ROAD = 1;
-const BLOCK_WALL = 2;
-const BLOCK_CITY = 3;
+	constructor(width: number, height: number) {
+		this.map = new MapGrid(width, height);
+		for(let i = 0; i < width; i++) {
+			for(let j = 0; j < height; j++) {
+				this.map.setTile(new Coord(i, j), 'UNKNOWN');
+			}
+		}
+	}
 
-const salesman = {
-	x: 0,
-	y: 0
+	isExplored(coord: Coord) {
+		return this.map.getTile(coord).type !== 'UNKNOWN';
+	}
+
+	setInitialCoord(coord: Coord, tile: MapTile) {
+		this.coord = coord;
+		// must be unexplored!
+		//this.map.setTile(coord, tile.type);
+	}
+	
+	exploreTile(tile: MapTile) {
+		this.map.setTile(tile.coord, tile.type);
+	}
+
+	goto(newCoord: Coord) {
+		if(!this.map.getTile(newCoord).isWalkable) {
+			throw new Error('I can\'t go there!');
+		}
+		this.coord = newCoord;
+	}
 }
+
+const salesman = new Salesman(WIDTH, HEIGHT);
+const realMap = new MapGrid(WIDTH, HEIGHT);
 
 for (let i = 0; i < WIDTH; i++) {
 	for (let j = 0; j < HEIGHT; j++) {
 		const rand = Math.random();
 		if (rand > 0.75) {
 			if (rand > 0.9) {
-				arr.push(BLOCK_CITY);
+				realMap.setTile(new Coord(i, j), 'CITY');
 			} else {
-				arr.push(BLOCK_WALL);
+				realMap.setTile(new Coord(i, j), 'WALL');
 			}
 		} else {
-			arr.push(BLOCK_ROAD);
+			realMap.setTile(new Coord(i, j), 'ROAD');
 		}
 	}
 }
-
+realMap.generateNeighbors();
+realMap.print();
 
 
 /*
@@ -70,190 +258,109 @@ arr = [
 	3, 1, 2, 1, 1, 1,
 ];*/
 
-const printArr = (array = arr) => {
-	let otp = "";
-	array.forEach((val, index) => {
-		otp += `${val}`;
-		if (index !== 0 && ((index + 1) % WIDTH) === 0) {
-			otp += '\n';
-		}
-	});
-	console.log(otp);
-}
-
-const coordToIndex = (x, y) => {
-	if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT) {
-		return -1;
-	}
-	return y * WIDTH + x;
-}
-
-const indexToCoord = (x) => {
-	return { x: x % WIDTH, y: Math.floor(x / WIDTH) };
-}
 
 const putSalesmanIntoMap = () => {
 	console.log('Put into map');
-	const cities = arr.filter(a => a === BLOCK_CITY).length;
+	const cities = realMap.mapArray.filter(a => a.type === 'CITY').length;
 	console.log('Cities: ' + cities);
 	//const randomLoc = Math.floor(Math.random() * cities);
 	const randomLoc = 2;
 	console.log('Random loc: ' + randomLoc);
 	let cnt = 0;
-	const index = arr.findIndex((val, index) => {
-		if (val === BLOCK_CITY && ++cnt === randomLoc) {
+	const cityTile = realMap.mapArray.find((val) => {
+		if (val.type === 'CITY' && ++cnt === randomLoc) {
 			return true;
 		}
-		//cnt++;
 	});
-	const coord = indexToCoord(index);
-	salesman.x = coord.x;
-	salesman.y = coord.y;
-	console.log('Found pos: ', index, salesman.x, salesman.y);
+	salesman.setInitialCoord(cityTile.coord, cityTile);
+	console.log('Found pos: ', salesman.coord);
 }
 
-const getNeighbors = (x, y, diagonal = true) => {
-	if (diagonal) {
-		return [
-			arr[coordToIndex(x - 1, y - 1)], arr[coordToIndex(x, y - 1)], arr[coordToIndex(x + 1, y - 1)],
-			arr[coordToIndex(x - 1, y)], arr[coordToIndex(x, y)], arr[coordToIndex(x + 1, y)],
-			arr[coordToIndex(x - 1, y + 1)], arr[coordToIndex(x, y + 1)], arr[coordToIndex(x + 1, y + 1)]
-		].map(v => v ?? BLOCK_UNDEFINED)
-	} else {
-		return [
-			BLOCK_UNDEFINED, arr[coordToIndex(x, y - 1)], BLOCK_UNDEFINED,
-			arr[coordToIndex(x - 1, y)], arr[coordToIndex(x, y)], arr[coordToIndex(x + 1, y)],
-			BLOCK_UNDEFINED, arr[coordToIndex(x, y + 1)], BLOCK_UNDEFINED
-		].map(v => v ?? BLOCK_UNDEFINED)
-	}
-}
 
 const exploreMap = () => {
-	let exarr = [];
-	for (let i = 0; i < WIDTH; i++) {
-		for (let j = 0; j < HEIGHT; j++) {
-			exarr.push(BLOCK_UNDEFINED);
-		}
-	}
+	salesman.map.print();
 
-	exarr[coordToIndex(salesman.x, salesman.y)] = BLOCK_CITY;
-	printArr(exarr);
-
-	const NODE_NEW = 0;
-	const NODE_EXPLORED = 1;
-	const helpArray = [...exarr].map(val => NODE_NEW);
-	const stack = [];
-	stack.push(salesman);
+	// todo use better structure
+	const stack: Coord[] = [];
+	stack.push(salesman.coord);
 
 	console.log('Searching');
-	printArr(helpArray);
 	while (stack.length !== 0) {
 		// pop
 		const qCoord = stack[stack.length - 1];
 		stack.splice(stack.length - 1, 1);
-		const qCoordIndex = coordToIndex(qCoord.x, qCoord.y);
 
-		if (helpArray[qCoordIndex] !== NODE_EXPLORED) {
-			helpArray[qCoordIndex] = NODE_EXPLORED;
-			// explore map
-			exarr[qCoordIndex] = arr[qCoordIndex];
+		if (!salesman.isExplored(qCoord)) {
+			const tile = realMap.getTile(qCoord);
+			salesman.exploreTile(tile);
 
-			// get all neighbors including diagonals
-			const neighbors = getNeighbors(qCoord.x, qCoord.y, true);
+			// get all neighbors, for now without diagonals
+			const neighbors = tile.directionalNeighbors;
 			neighbors.forEach((val, index) => {
-				// get neighbor coord
-				const coord = { x: index % 3, y: Math.floor(index / 3) };
-				const isDiagonal = index === 0 || index === 2 || index === 6 || index === 8;
-				const isCurrent = index === 4; // we can skip the current node
-
-				if (!isCurrent && val !== BLOCK_UNDEFINED) {
-					const bigMapCoord = { // [1, 1] is the center
-						x: qCoord.x + coord.x - 1,
-						y: qCoord.y + coord.y - 1
-					};
-					const bigMapIndex = coordToIndex(bigMapCoord.x, bigMapCoord.y);
-					if (!isDiagonal && helpArray[bigMapIndex] !== NODE_EXPLORED) {
-						if (val !== BLOCK_WALL) {
-							stack.push(bigMapCoord);
-						}
-					}
-					// we can look diagonally, but can't go there
-					// if there is a wall, we can ignore it
-					if (isDiagonal && val === BLOCK_WALL) {
-						helpArray[bigMapIndex] = NODE_EXPLORED;
-						salesman.x = bigMapCoord.x;
-						salesman.y = bigMapCoord.y;
-						exarr[bigMapIndex] = arr[bigMapIndex];
-					}
+				if(val && !salesman.isExplored(val.coord)) {
+					stack.push(val.coord);
 				}
 			});
 		}
-
-		printArr(helpArray);
 	}
-	printArr(exarr);
+
+	salesman.map.generateNeighbors();
+	salesman.map.print();
 
 	// at this moment, the salesman knows the map
 	// let's use dijsktra to find the shortest path from A to B
-
 }
 
 
-const findPathWithDijkstra = (startCoord, endCoord, arr) => {
+const findPathWithDijkstra = (startCoord: Coord, endCoord: Coord, map: MapGrid) => {
 	console.log('Running Dijkstra', startCoord, endCoord);
 	let steps = {};
 	let backtrace = {};
 	let pq = new PriorityQueue();
 
-	const startIndex = coordToIndex(startCoord.x, startCoord.y);
-	const endIndex = coordToIndex(endCoord.x, endCoord.y);
+	const startIndex = map.coordToIndex(startCoord);
+	const endIndex = map.coordToIndex(endCoord);
 
 	steps[startIndex] = 0;
 
-	arr.forEach((val, index) => {
+	map.mapArray.forEach((val) => {
+		// todo we could use index from the iterator as well
+		const index = map.coordToIndex(val.coord);
 		if (index !== startIndex) {
 			steps[index] = Infinity;
 		}
 	});
 
-	pq.enqueue([startIndex, 0]);
+	pq.enqueue(startCoord, 0);
 
-	while (!pq.isEmpty()) {
-		let currentIndex = pq.dequeue()[0];
-		let currentCoord = indexToCoord(currentIndex);
-
-		const neighbors = getNeighbors(currentCoord.x, currentCoord.y, false);
-		// skip the point where we actually are
-		neighbors.forEach((val, index) => {
-			if (index !== 4 && val !== BLOCK_UNDEFINED && val !== BLOCK_WALL) {
-				const coord = { x: index % 3, y: Math.floor(index / 3) };
-				const bigMapCoord = { // [1, 1] is the center
-					x: currentCoord.x + coord.x - 1,
-					y: currentCoord.y + coord.y - 1
-				};
-				const bigMapIndex = coordToIndex(bigMapCoord.x, bigMapCoord.y);
-
+	while (!pq.isEmpty) {
+		let currentCoord = pq.dequeue()[0];
+		let currentIndex = map.coordToIndex(currentCoord);
+		const neighbors = map.getTile(currentCoord).directionalNeighbors;
+		neighbors.forEach((val) => {
+			if (val && val.isWalkable) {
+				const neighbourIndex = map.coordToIndex(val.coord);
 				let price = steps[currentIndex] + 1;
-				if (price < steps[bigMapIndex]) {
-					steps[bigMapIndex] = price;
-					backtrace[bigMapIndex] = currentIndex;
-					pq.enqueue([bigMapIndex, price]);
+				if (price < steps[neighbourIndex]) {
+					steps[neighbourIndex] = price;
+					backtrace[neighbourIndex] = currentIndex;
+					pq.enqueue(val.coord, price);
 				}
 			}
 		});
 	}
-	let path = [indexToCoord(endIndex)];
+
+	let path: Coord[] = [endCoord];
 	let lastStep = endIndex;
 	while (lastStep && lastStep !== startIndex) {
-		path.unshift(indexToCoord(backtrace[lastStep]));
+		path.unshift(map.indexToCoord(backtrace[lastStep]));
 		lastStep = backtrace[lastStep];
 	}
 	console.log(path);
 	console.log(steps[endIndex]);
 }
 
-printArr();
 putSalesmanIntoMap();
-const firstSalesmanLoc = { ...salesman };
+const originalCoord = salesman.coord;
 exploreMap();
-findPathWithDijkstra(salesman, firstSalesmanLoc, arr);
+findPathWithDijkstra(originalCoord, salesman.coord, salesman.map);
